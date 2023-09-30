15375dc0898daf6f00685cb083af31dff99c8bd1
AlexandreIorio <alexandre.iorio@heig-vd.ch>
2023-09-30 08:18:21 +0200

    WORK: Refactor

--- src/main/java/ch/heigvd/Inko.java
+++ src/main/java/ch/heigvd/Inko.java
@@ -4,7 +4,6 @@ import com.drew.imaging.ImageProcessingException;
 import picocli.CommandLine;
 
 import java.awt.image.BufferedImage;
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
@@ -13,20 +12,18 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(name = "Inko", mixinStandardHelpOptions = true, version = "1.0", description = "Inko lets you write custom text or exif values on an image")
 
 public class Inko implements Callable {
-    private static int cptCmdText = 0;
-
+    private static int _cptCmdText = 0;
     private static ImgHandler _imgHandler;
     private static ExifHandler _exifHandler;
     private static ImageTextOverlay _overlayer;
     private static String _imagePath;
 
-
-    @CommandLine.Option(names = {"-cr","--credits"}, description = "Show credits")
+    @CommandLine.Option(names = {"-cr", "--credits"}, description = "Show credits")
     public void ShowCredit(boolean param) {
         System.out.println(Description.DESCRIPTION);
     }
 
-    @CommandLine.Option(names = {"-p", "--imagePath"}, description = "Image path")
+    @CommandLine.Option(names = {"-p", "--imagePath"}, description = "Image source path")
     private static void setImagePath(String param) throws ImageProcessingException, IOException {
         _imagePath = param;
         _imgHandler = new ImgHandler(param);
@@ -34,27 +31,13 @@ public class Inko implements Callable {
     }
 
     @CommandLine.Option(names = {"-o", "--output"}, description = "Output path without extension", defaultValue = "OverlayedImage")
-    private static String _outputPath = "OverlayedImage";
+    private String _outputPath;
 
-    @CommandLine.Option(names = {"-of", "--outputformat"}, description = "Output format : jpeg, png, gif")
-    private static String _outputFormat = "jpeg";
-
-    @CommandLine.Option(names = {"-t", "--text"}, description = "Text to overlay")
-    private void addText(ArrayList<String> params) {
-        if (_imgHandler == null) throw new NullPointerException("no image path has been given");
-        if(params.isEmpty()) return;
-        _exifHandler.AddText(params.get(cptCmdText));
-        cptCmdText++;
-    }
-
-    @CommandLine.Option(names = {"-d", "--date"}, description = "Get date of image")
-    void addDate(boolean called) {
-        if (_exifHandler == null) throw new NullPointerException("no image path has been given");
-        _exifHandler.AddExifData(ExifHandler.EXIF.DateOriginal);
-    }
+    @CommandLine.Option(names = {"-of", "--outputformat"}, description = "Output format : jpeg, png, gif", defaultValue = "jpeg")
+    private String _outputFormat;
 
     @CommandLine.Option(names = {"-df", "--dateformat"}, description = "Set the format of the date")
-        void setDateFormat(String param) {
+    void setDateFormat(String param) {
         if (_exifHandler == null) throw new NullPointerException("no image path has been given");
         _exifHandler.SetDateFormat(param);
     }
@@ -65,13 +48,32 @@ public class Inko implements Callable {
         _exifHandler.SetGMT(Integer.parseInt(param));
     }
 
-    @CommandLine.Option(names = {"-cm", "--cam_model"}, description = "Get model of camera")
+    @CommandLine.Option(names = {"-t", "--text"}, description = "Text to overlay")
+    private void addText(ArrayList<String> params) {
+        if (_imgHandler == null) throw new NullPointerException("no image path has been given");
+        if (params.isEmpty()) return;
+        _exifHandler.AddText(params.get(_cptCmdText));
+        _cptCmdText++;
+    }
+
+    @CommandLine.Option(names = {"-s", "--sep"}, description = "Data separator")
+    private static void SetSeparator(String param) {
+        ExifHandler.separator = " " + param + " ";
+    }
+
+    @CommandLine.Option(names = {"-d", "--date"}, description = "Get date from EXIF data ")
+    void addDate(boolean called) {
+        if (_exifHandler == null) throw new NullPointerException("no image path has been given");
+        _exifHandler.AddExifData(ExifHandler.EXIF.DateOriginal);
+    }
+
+    @CommandLine.Option(names = {"-cm", "--cam_model"}, description = "Get model of camera from EXIF data")
     void addCamModel(boolean called) {
         if (_imgHandler == null) throw new NullPointerException("no image path has been given");
         _exifHandler.AddExifData(ExifHandler.EXIF.CameraModel);
     }
 
-    @CommandLine.Option(names = {"-gps", "--gps_location"}, description = "Get gps location of image")
+    @CommandLine.Option(names = {"-gps", "--gps_location"}, description = "Get gps location from EXIF data")
     void addGpsLocation(boolean called) {
         if (_imgHandler == null) throw new NullPointerException("no image path has been given");
         _exifHandler.AddExifData(ExifHandler.EXIF.GPSLocation);
@@ -88,12 +90,12 @@ public class Inko implements Callable {
         _overlayer.SetFont(param);
     }
 
-    @CommandLine.Option(names = {"-fw", "--fontwidth"}, description = "font width : bold, italic, plain", defaultValue = "bold")
+    @CommandLine.Option(names = {"-fw", "--fontwidth"}, description = "font width : bold, italic, plain")
     private void setFontWidth(String param) {
         _overlayer.SetFontWidth(param);
     }
 
-    @CommandLine.Option(names = {"-fs", "--fontSize"}, description = "font size px", defaultValue = "50")
+    @CommandLine.Option(names = {"-fs", "--fontSize"}, description = "font size px")
     private void setFontSize(String param) {
         _overlayer.SetFontSize(param);
     }
@@ -103,14 +105,14 @@ public class Inko implements Callable {
         _overlayer.SetColor(param);
     }
 
+    @CommandLine.Option(names = {"-ma", "--margin"}, description = "Margin of overlaid image/text")
+    private void setMargin(String param) {
+        _overlayer.SetMargin(param);
+    }
+
     @CommandLine.Option(names = {"-bg", "--backgroundcolor"}, description = "background color:  #AARRGGBB")
     private void setBackgroundColor(String param) {
         _overlayer.SetBackgroundColor(param);
-    }
-
-    @CommandLine.Option(names = {"-s", "--sep"}, description = "Data separator")
-    private static void SetSeparator(String param) {
-        ExifHandler.separator = " " + param + " ";
     }
 
     @CommandLine.Option(names = {"-sh", "--show"}, description = "Show image after generation")
