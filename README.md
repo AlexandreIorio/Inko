1943ee3ca407512263cbceca99ea028d56077e59
AlexandreIorio <alexandre.iorio@heig-vd.ch>
2023-09-30 10:55:04 +0200

    ADD: Inko introduction

--- README.md
+++ README.md
@@ -1,8 +1,130 @@
-# JavaInkOverlay
-JavaInkOverlay: Add text and drawings to your photos effortlessly.
+# Inko - Custom Text and Exif Data Overlay on Images
+
+**Version**: 1.0
+![Forest Gump](data/fgo.jpeg)
+```
+java -jar target/InkoCli-1.0-SNAPSHOT.jar -p data/fg.jpg -t "Life is like a box of chocolates. You never know what you're gonna get." -f "z003" -bg "#80FFFFFF" -po c -fs 35 -o data/fgo -sh
+```
+
+## Prerequisites
+
+Inko is a Java program that can be built using Maven, a popular build automation and project management tool. Follow the steps below to compile your Java code, package it into a JAR file, and build your project using Maven.
+
+- Java Development Kit (JDK)
+- Maven
+  
+## Description
+
+Inko is a command-line tool that allows you to add custom text or Exif (Exchangeable Image File Format) data to an image. You can use it to annotate your images with various information or labels.
+
+## Usage
+
+```
+java -jar Inko.jar [OPTIONS]
+```
+
+## Options
+
+### File 
+
+- `-p, --imagePath PATH`  
+  Specifies the path to the input image.
+
+- `-o, --output PATH`  
+  Specifies the output path and file name (without extension) for the overlaid image. (Default: OverlayedImage)
+
+- `-of, --outputformat FORMAT`  
+  Specifies the output image format (jpeg, png, gif). (Default: jpeg)
+
+- `-sh, --show`  
+  Show the overlaid image after processing.
+
+  ## Overlay data specification
+
+  `-t, --text TEXT`  
+  Specify text to overlay on the image. You can use this option multiple times to add multiple lines of text.
+
+- `-d, --date`  
+  Get the date of the image and overlay it.
+
+- `-df, --dateformat FORMAT`  
+  Set the format of the date.
+
+- `-gmt, --gmt OFFSET`  
+  Set the GMT (Greenwich Mean Time) offset.
+
+- `-cm, --cam_model`  
+  Get the model of the camera that took the image.
+
+- `-gps, --gps_location`  
+  Get the GPS location of the image.
+
+- `-is, --imagesize`  
+  Get the size of the image in pixels.
+
+- `-s, --sep SEPARATOR`  
+  Specifies the separator for data when adding Exif values.
+
+## Overlay format
+
+- `-po, --position POSITION`  
+  Specifies the position of the overlaid text. Options include l, r, b, t, c, lt, rt, lb, rb. (Default: rb)
+
+- `-f, --font FONT`  
+  Specify the font to be used for text overlay. (Default: Arial)
+
+- `-fw, --fontwidth WIDTH`  
+  Specify the font width (bold, italic, plain). (Default: bold)
+
+- `-fs, --fontSize SIZE`  
+  Specify the font size in pixels. (Default: 50)
+
+- `-fc, --fontcolor COLOR`  
+  Specify the font color in the format #RRGGBB (e.g., #2e00ff).
+
+- `-bg, --backgroundcolor COLOR`  
+  Specify the background color in the format #AARRGGBB.
+  
+
+## Other options
+
+- `-cr, --credits`  
+  Show program credits.
+
+- `-h, --help`  
+  Show program help and description.
+
+## Examples
 
 
-# Work in progress
+1. Overlay text on an image:
+   ```
+   java -jar target/InkoCli-1.0-SNAPSHOT.jar -p data/img.jpg -t "What does the cow say ?" -po c -fc "#FFFF0000" -fs 150 -bg "#80FFFF00" -o output.jpg -sh
+   ```
+   this commande launch `target/InkoCli-1.0-SNAPSHOT.jar`, get `data/img.jpg`, and apply a textual `-t "What does the cow say ?"` at the center of the image `-po c` with à red font color opacity 100% `-fc "#FFFF0000"` and size 150px `-fs 150`  with a yellow background opacity 50% `"#80FFFF00"`. The output file name is output.jpg `-o output.jpg` and the image will pop up after the process `-sh`
 
-# Dependency 
-https://github.com/drewnoakes/metadata-extractor
+2. Show program credits:
+   ```
+   java -jar target/InkoCli-1.0-SNAPSHOT.jar -cr
+   ```
+
+3. Overlay the date on an image:
+   ```
+   java -jar target/InkoCli-1.0-SNAPSHOT.jar -p data/img.jpg -d -bg "#80FFFFFF" -o output.jpg -sh
+   ```
+
+4. Get the camera model,  GPS location and the image size included personal texts:
+   ```
+   java -jar target/InkoCli-1.0-SNAPSHOT.jar -p data/img.jpg -t "Camera: " -cm -t "GPS : " -gps -is -bg "#80FFFFFF" -o output.jpg -sh
+   ```
+
+
+# Dependencies
+## metadata-extractor:
+Metadata-Extractor is a Java library for reading metadata from media files.
+[https://github.com/drewnoakes/metadata-extractor](https://github.com/drewnoakes/metadata-extractor)
+
+## Picocli
+Picocli aims to be the easiest way to create rich command line applications that can run on and off the JVM : [https://github.com/remkop/picocli](https://github.com/remkop/picocli)
+
+*Documentation on [https://picocli.info/](https://picocli.info/)*
--- src/main/java/ch/heigvd/Inko.java
+++ src/main/java/ch/heigvd/Inko.java
@@ -13,36 +13,67 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(name = "Inko", mixinStandardHelpOptions = true, version = "1.0", description = "Inko lets you write custom text or exif values on an image")
 
 public class Inko implements Callable {
+    /**
+     * counter for sequence of text
+     */
     private static int cptCmdText = 0;
-
+    /**
+     * Image handler
+     */
     private static ImgHandler _imgHandler;
+    /**
+     * Exif handler
+     */
     private static ExifHandler _exifHandler;
+    /**
+     * Image/Text overlayer
+     */
     private static ImageTextOverlay _overlayer;
+    /**
+     * Image path
+     */
     private static String _imagePath;
-
-
-    @CommandLine.Option(names = {"-cr","--credits"}, description = "Show credits")
-    public void ShowCredit(boolean param) {
-        System.out.println(Description.DESCRIPTION);
-    }
-
     @CommandLine.Option(names = {"-p", "--imagePath"}, description = "Image path")
     private static void setImagePath(String param) throws ImageProcessingException, IOException {
         _imagePath = param;
         _imgHandler = new ImgHandler(param);
         _exifHandler = new ExifHandler(param);
     }
-
     @CommandLine.Option(names = {"-o", "--output"}, description = "Output path without extension", defaultValue = "OverlayedImage")
     private static String _outputPath = "OverlayedImage";
-
     @CommandLine.Option(names = {"-of", "--outputformat"}, description = "Output format : jpeg, png, gif")
     private static String _outputFormat = "jpeg";
+    @CommandLine.Option(names = {"-sh", "--show"}, description = "Show image after generation")
+    boolean _showImage = false;
+    @CommandLine.Option(names = {"-po", "--position"}, description = "Text Position: l, r, b, t, c, lt, rt, lb, rb", defaultValue = "rb")
+    private static String _position = "rb";
+
+
+    @CommandLine.Option(names = {"-s", "--sep"}, description = "Data separator")
+    private static void SetSeparator(String param) {
+        ExifHandler.separator = " " + param + " ";
+    }
+
+    /**
+     * program input on the command
+     *
+     * @param args arguments
+     */
+    public static void main(String[] args) throws Exception {
+        _overlayer = new ImageTextOverlay();
+        int exitCode = new CommandLine(new Inko()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @CommandLine.Option(names = {"-cr", "--credits"}, description = "Show credits")
+    public void ShowCredit(boolean param) {
+        System.out.println(Description.DESCRIPTION);
+    }
 
     @CommandLine.Option(names = {"-t", "--text"}, description = "Text to overlay")
     private void addText(ArrayList<String> params) {
         if (_imgHandler == null) throw new NullPointerException("no image path has been given");
-        if(params.isEmpty()) return;
+        if (params.isEmpty()) return;
         _exifHandler.AddText(params.get(cptCmdText));
         cptCmdText++;
     }
@@ -54,7 +85,7 @@ public class Inko implements Callable {
     }
 
     @CommandLine.Option(names = {"-df", "--dateformat"}, description = "Set the format of the date")
-        void setDateFormat(String param) {
+    void setDateFormat(String param) {
         if (_exifHandler == null) throw new NullPointerException("no image path has been given");
         _exifHandler.SetDateFormat(param);
     }
@@ -108,17 +139,6 @@ public class Inko implements Callable {
         _overlayer.SetBackgroundColor(param);
     }
 
-    @CommandLine.Option(names = {"-s", "--sep"}, description = "Data separator")
-    private static void SetSeparator(String param) {
-        ExifHandler.separator = " " + param + " ";
-    }
-
-    @CommandLine.Option(names = {"-sh", "--show"}, description = "Show image after generation")
-    boolean _showImage = false;
-    @CommandLine.Option(names = {"-po", "--position"}, description = "Text Position: l, r, b, t, c, lt, rt, lb, rb", defaultValue = "rb")
-    private static String _position = "rb";
-
-
     @Override
     public Integer call() throws Exception {
         if (_imgHandler == null) return 0;
@@ -133,17 +153,6 @@ public class Inko implements Callable {
             ImgHandler.openImage(_outputPath + '.' + _outputFormat);
         }
         return 0;
-    }
-
-    /**
-     * program input on the command
-     *
-     * @param args arguments
-     */
-    public static void main(String[] args) throws Exception {
-        _overlayer = new ImageTextOverlay();
-        int exitCode = new CommandLine(new Inko()).execute(args);
-        System.exit(exitCode);
     }
 
 }
